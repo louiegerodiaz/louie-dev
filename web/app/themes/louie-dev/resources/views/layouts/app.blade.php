e0ea81c2912e0ef551a644b7abe6e4bcc2c36de6
Louie Gerodiaz <louiegerodiaz@gmail.com>
2020-04-25 16:30:27 -0700

    started developing home page

--- web/app/themes/louie-dev/resources/views/layouts/app.blade.php
+++ web/app/themes/louie-dev/resources/views/layouts/app.blade.php
@@ -4,20 +4,13 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
-      </div>
+    <div class="content" role="document">
+      <main class="main">
+        @yield('content')
+      </main>
     </div>
     @php do_action('get_footer') @endphp
-    @include('partials.footer')
+    {{-- @include('partials.footer') --}}
     @php wp_footer() @endphp
   </body>
 </html>
--- web/app/themes/louie-dev/resources/views/partials/header.blade.php
+++ web/app/themes/louie-dev/resources/views/partials/header.blade.php
@@ -1,10 +1,12 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+<header class="side">
+  <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+  <nav class="nav-primary">
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+    @endif
+  </nav>
+  <div class="contact">
+    <p><a class="link-underline" href="mailto:hello@louie.dev">hello@louie.dev</a></p>
+    <p>Pasig City, Manila, PH</p>
   </div>
 </header>
